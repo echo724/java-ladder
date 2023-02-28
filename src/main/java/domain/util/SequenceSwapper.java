cb9b29a13f35150e34ffb2d603bb77f95a67db64
echo <echo.backend@gmail.com>
2023-03-01 01:30:51 +0900

    fix(SequenceSwapper): of 메서드 제거 및 배열 삭제

--- src/main/java/domain/util/SequenceSwapper.java
+++ src/main/java/domain/util/SequenceSwapper.java
@@ -1,7 +1,6 @@
 package domain.util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,21 +11,17 @@ public class SequenceSwapper {
     private static final String INDEX_OUT_OF_BOUND_ERROR_MESSAGE = "교환 가능한 인덱스는 순서보다 1 작아야합니다.";
     private final List<Integer> sequence;
     
-    private SequenceSwapper( List<Integer> numbers ) {
+    private SequenceSwapper(List<Integer> numbers) {
         this.sequence = new ArrayList<>(numbers);
     }
     
-    public static SequenceSwapper initialize( int length ) {
-        int[] numbers = IntStream.range(0, length).toArray();
-        return SequenceSwapper.of(numbers);
+    public static SequenceSwapper initialize(int length) {
+        List<Integer> numbers = IntStream.range(0, length).boxed().collect(Collectors.toList());
+        return new SequenceSwapper(numbers);
     }
     
-    public static SequenceSwapper of( int... numbers ) {
-        return new SequenceSwapper(Arrays.stream(numbers).boxed().collect(Collectors.toList()));
-    }
-    
-    public void swap( int index ) {
-        if ( index >= this.sequence.size() - 1 ) {
+    public void swap(int index) {
+        if (index >= this.sequence.size() - 1) {
             throw new IndexOutOfBoundsException(INDEX_OUT_OF_BOUND_ERROR_MESSAGE);
         }
         int temporary = this.sequence.get(index);
